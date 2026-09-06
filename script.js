8c3e78ada215f30e2c775ab3f7430d59e6e55a8c
Dpka Singh <112195696+Ddeepika123@users.noreply.github.com>
2026-09-07 00:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,26 @@
 
 function palindrome(str){
 
+    // Convert string to lowercase
+    str = str.toLowerCase();
+
+    // Remove spaces
+    str = str.replace(/\s/g, "");
+
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+
+        if (str[left] !== str[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
 }
+
 module.exports = palindrome
